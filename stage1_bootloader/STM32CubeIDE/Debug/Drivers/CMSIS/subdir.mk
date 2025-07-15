f7cfdd57c127387c46274cbcf6c450d66089ac63
kashish55991 <kshah@digi.com>
2025-07-15 18:17:45 -0500

    Initial Commit. Created STM32 Cube IDE Projects.

---
+++ stage1_bootloader/STM32CubeIDE/Debug/Drivers/CMSIS/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (13.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+C:/Kashish/Projects/STM32\ UART\ Bootloader/stm32-uart-bootloader/stage1_bootloader/Core/Src/system_stm32l4xx.c 
+
+OBJS += \
+./Drivers/CMSIS/system_stm32l4xx.o 
+
+C_DEPS += \
+./Drivers/CMSIS/system_stm32l4xx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Drivers/CMSIS/system_stm32l4xx.o: C:/Kashish/Projects/STM32\ UART\ Bootloader/stm32-uart-bootloader/stage1_bootloader/Core/Src/system_stm32l4xx.c Drivers/CMSIS/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32L476xx -DUSE_FULL_LL_DRIVER -DHSE_VALUE=8000000 -DHSE_STARTUP_TIMEOUT=100 -DLSE_STARTUP_TIMEOUT=5000 -DLSE_VALUE=32768 -DMSI_VALUE=4000000 -DEXTERNALSAI1_CLOCK_VALUE=2097000 -DEXTERNALSAI2_CLOCK_VALUE=2097000 -DHSI_VALUE=16000000 -DLSI_VALUE=32000 -DVDD_VALUE=3300 -DPREFETCH_ENABLE=1 -DINSTRUCTION_CACHE_ENABLE=1 -DDATA_CACHE_ENABLE=1 -c -I../../Core/Inc -I../../Drivers/STM32L4xx_HAL_Driver/Inc -I../../Drivers/CMSIS/Device/ST/STM32L4xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"Drivers/CMSIS/system_stm32l4xx.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Drivers-2f-CMSIS
+
+clean-Drivers-2f-CMSIS:
+	-$(RM) ./Drivers/CMSIS/system_stm32l4xx.cyclo ./Drivers/CMSIS/system_stm32l4xx.d ./Drivers/CMSIS/system_stm32l4xx.o ./Drivers/CMSIS/system_stm32l4xx.su
+
+.PHONY: clean-Drivers-2f-CMSIS
+
